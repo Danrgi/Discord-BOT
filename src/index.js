b77119b9ab2f113cd19a20fd733c7901a1d0b9b6
Dan Gimenes <81711842+Danrgi@users.noreply.github.com>
2025-11-19 15:49:17 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits, Message } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once('ready', () => {
+    console.log('The BOT is now Online!')
+});
+
+client.login(process.env.BOT_TOKEN);
